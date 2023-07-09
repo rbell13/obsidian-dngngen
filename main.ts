a192f2c9ae090cc147bd93000886ef656a189073
Roland <rbell13@gmu.edu>
2023-07-08 20:38:54 -0400

    first push. not very pretty table, type has to be DNGNGEN

--- main.ts
+++ main.ts
@@ -1,137 +1,59 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+export default class DngngenPlugin extends Plugin {
+    async onload() {
+      this.registerMarkdownCodeBlockProcessor("DNGNGEN", (source, el, ctx) => {
+        const splitByAttribution = source.split("_________________");
+        const attribution = splitByAttribution[1]
+        const text = splitByAttribution[2]
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+        const regex = /\b([A-Z ]+)\b/;
+        const dngnName: RegExpExecArray | null = regex.exec(text);
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+        let remainingText: string | null = null;
+        if (dngnName) {
+            // title
+            el.createEl("h2", {text: dngnName[0]});
 
-	async onload() {
-		await this.loadSettings();
+            const wordIndex: number = dngnName.index || 0;
+            remainingText = text.substring(wordIndex + dngnName[0].length).trim();
+        }
+        
+        if (remainingText) {
+            const headers = [
+                "What brings you here?",
+                "Status",
+                "Imminent danger",
+                "Who or what dwells here now?",
+                "Entrance",
+                "Guarded by",
+                "Distinctive feature",
+                "Room 1",
+                "Room 2",
+                "Room 3",
+                "Room 4"
+            ];
+            const table = el.createEl("table");
+            const body = table.createEl("tbody");
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+            headers.forEach((header) => {
+                const escapedHeader = header.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+                const regex = new RegExp(`${escapedHeader}\\s*(.+?)(?:\\. |$)`);
+                const match = text.match(regex);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+                const row = body.createEl("tr");
+                if (match) {
+                  row.createEl("td", {text: header});
+                  row.createEl("td", {text: match[1].trim() + "."});
+                }else {
+                    const row = body.createEl("tr");
+                    row.createEl("td", { text: header });
+                    row.createEl("td", { text: "" }); // Add an empty cell if no match is found
+                  }
+              });
+        }
+        el.createEl("sub", {text: attribution})
+      });
+    }
+  }
